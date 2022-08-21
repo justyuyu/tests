269f911a075c0f379e8873a1b2adab11cb6ee9d2
justyuyu <102282922+justyuyu@users.noreply.github.com>
2022-08-21 22:46:44 +0800

    Update 1_helloworld.sol

--- 1_helloworld.sol
+++ 1_helloworld.sol
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
+//第一课 hello world；
 pragma solidity ^0.8.0;
 
 contract helloworld {
@@ -9,5 +10,12 @@ contract helloworld {
     function changeName(string memory _newName)public{
         Myname = _newName;
     }
-}
+
 //public:任何人都可以调用该函数，包括Dapp的创建者；
+
+//第二课 节约gas的利器 pure 和 view；
+    function pureTest(string memory _Name)pure public returns(string memory){
+        return _Name;
+    }
+}
+//pure 不耗费gas费用；
